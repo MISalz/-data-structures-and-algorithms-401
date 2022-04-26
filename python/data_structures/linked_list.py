97194f21ef74f2c6f6453009a69462c043dec369
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-04-25 23:08:03 -0700

    new

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -5,11 +5,38 @@ class LinkedList:
 
     def __init__(self):
         # initialization here
-        pass
+        self.value = None
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def __str__(self):
+        string = ""
+        if self.head ==None:
+            return 'NULL'
+        current = self.head
+        while current:
+            string += f"{{ {current.value} }} -> "
+            current = current.next
+        string += 'NULL'
+        return string
+
+    def insert(self, value):
+        self.head = Node(value, self.head)
+        self.value = str(value)
+
+
+    def includes(self, target_value):
+        current = self.head
+        while current:
+            if current.value == target_value:
+                return True
+            current = current.next
+
+        return False
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
 
 
 class TargetError:
