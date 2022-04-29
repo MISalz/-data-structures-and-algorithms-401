f7c79e728565f0168500177fadee3db1ed727e32
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-04-29 16:56:37 -0700

    starting lab 10

--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,6 +1,11 @@
 class Stack:
     """
-    Put docstring here
+    Create a Stack class that has a top property. It creates an empty Stack when instantiated.
+This object should be aware of a default empty value assigned to top when the stack is created.
+The class should contain the following methods:
+push
+Arguments: value
+adds a new node with that value to the top of the stack with an O(1) Time performance.
     """
 
     def __init__(self):
