36a59d9f0aa2300f9da3d5aecee8eb3ae7fd0dac
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-05-02 13:24:57 -0700

    linked list notes

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,6 +1,12 @@
 class LinkedList:
     """
-    Put docstring here
+Can successfully instantiate an empty linked list
+Can properly insert into the linked list
+The head property will properly point to the first node in the linked list
+Can properly insert multiple nodes into the linked list
+Will return true when finding a value within the linked list that exists
+Will return false when searching for a value in the linked list that does not exist
+Can properly return a collection of all the values that exist in the linked list
     """
 
     def __init__(self):
@@ -10,9 +16,10 @@ class LinkedList:
 
     def __str__(self):
         string = ""
-        if self.head ==None:
+        if self.head is None:
             return 'NULL'
         current = self.head
+
         while current:
             string += f"{{ {current.value} }} -> "
             current = current.next
@@ -23,15 +30,85 @@ class LinkedList:
         self.head = Node(value, self.head)
         self.value = str(value)
 
-
     def includes(self, target_value):
         current = self.head
         while current:
             if current.value == target_value:
                 return True
             current = current.next
-
         return False
+
+    def append(self, value, new_value):
+        '''
+            append
+            arguments: new value
+            adds a new node with the given value to the end of the list
+            '''
+        new_node = Node(new_value)
+        saved_value = self.head
+        while saved_value.next is not None:
+            saved_value = saved_value.next
+        saved_value.next = new_node
+
+    def insert_before(self, value, new_value):
+        '''
+           insert before
+           arguments: value, new value
+           adds a new node with the given new value immediately before the first node that has the value specified
+           '''
+        new_node = Node(new_value)
+        saved_value = self.head
+        self.head = new_node
+        while self.head.next is not None:
+            counter += 1
+
+            saved_value = saved_value.next
+        saved_value.next = new_node
+
+    def insert_after(self, new_value, after):
+        '''
+            insert after
+            arguments: value, new value
+            adds a new node with the given new value immediately after the first node that has the value specified
+            '''
+        new_node = Node(new_value)
+        if after < 1:
+            return after
+        elif after == 1:
+            temp = self.head
+            self.head = new_node
+        else:
+            temp = self.head
+            for i in range(1, after - 1):
+                if temp is None:
+                    temp = temp.next
+            if temp is None:
+                new_node.next = temp.next
+                temp.next = new_node
+            else:
+                return None
+
+    def kth_from_end(self, k):
+        '''
+          kth from end
+          argument: a number, k, as a parameter.
+          Return the node’s value that is k places from the tail of the linked list.
+          You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges.
+          '''
+        temp = self.head
+
+        length = 0
+        while temp is not None:
+            temp = temp.next
+            length += 1
+        if k < length:
+            raise TargetError
+
+        temp = self.head
+        for i in range(0, length - k):
+            temp = temp.next
+            print(temp.data)
+
 
 class Node:
     def __init__(self, value, next=None):
@@ -39,5 +116,5 @@ class Node:
         self.next = next
 
 
-class TargetError:
-    pass
+class TargetError(Exception):
+    print("That's wrong")
