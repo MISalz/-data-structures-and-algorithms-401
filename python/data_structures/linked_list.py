4311ba039baed98d1d7df4f8523ea8b2b091f313
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-05-02 21:20:32 -0700

    linked_list_insertion passing

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -13,6 +13,7 @@ Can properly return a collection of all the values that exist in the linked list
         # initialization here
         self.value = None
         self.head = None
+        self.tail = None
 
     def __str__(self):
         string = ""
@@ -38,70 +39,74 @@ Can properly return a collection of all the values that exist in the linked list
             current = current.next
         return False
 
-    '''
-    append
-    arguments: new value
-    adds a new node with the given value to the end of the list
-    '''
-
-    def append(self, value, new_value):
-        new_node = Node(new_value)
-        saved_value = self.head
-        while saved_value.next is not None:
-            saved_value = saved_value.next
-        saved_value.next = new_node
-    '''
-    insert before
-    arguments: value, new value
-    adds a new node with the given new value immediately before the first node that has the value specified
-    '''
-    def insert_before(self, value, new_value):
-        new_node = Node(new_value)
-        saved_value = self.head
-        self.head = new_node
-        while self.head.next is not None:
-            counter += 1
-
-            saved_value = saved_value.next
-        saved_value.next = new_node
-
-
-
-    '''
-    insert after
-    arguments: value, new value
-    adds a new node with the given new value immediately after the first node that has the value specified
-    '''
-
-
-    def insert_after(self, new_value, after):
-        new_node = Node(new_value)
-        if after < 1:
-            return after
-        elif after == 1:
-            temp = self.head
+    def append(self, value):
+        '''
+        append
+        arguments: new value
+        adds a new node with the given value to the end of the list
+        '''
+        new_node = Node(value)
+        if self.head is None:
             self.head = new_node
-        else:
-            temp = self.head
-            for i in range(1, after - 1):
-                if temp is None:
-                    temp = temp.next
-            if temp is None:
-                new_node.next = temp.next
-                temp.next = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+
+    def insert_before(self, value, new_value):
+        '''
+           insert before
+           arguments: value, new value
+           adds a new node with the given new value immediately before the first node that has the value specified
+           '''
+        if self.head is None:
+            raise TargetError
+        if self.includes(value) is False:
+            raise TargetError
+
+        new_node = Node(new_value)
+        current = self.head
+        if current.value is value:
+            new_node.next = self.head
+            self.head = new_node
+
+        while current.next is not None:
+            if current.next.value is value:
+                new_node.next = current.next
+                current.next = new_node
+                return
             else:
-                return None
+                current = current.next
 
+    def insert_after(self, value, new_value):
+        '''
+           insert after
+           arguments: value, new value
+           adds a new node with the given new value immediately after the first node that has the value specified
+           '''
+        current = self.head
+        if self.head is None:
+            raise TargetError
+        if self.includes(value) is False:
+            raise TargetError
+        while current.next is not None:
+            if current.value is value:
+                current.next = Node(new_value, current.next)
+                break
+            else:
+                current = current.next
 
-    '''
-    kth from end
-    argument: a number, k, as a parameter.
-    Return the node’s value that is k places from the tail of the linked list.
-    You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges.
-    '''
 
 
     def kth_from_end(self, k):
+        '''
+           kth from end
+           argument: a number, k, as a parameter.
+           Return the node’s value that is k places from the tail of the linked list.
+           You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges.
+           '''
         temp = self.head
 
         length = 0
