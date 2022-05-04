a89711067b7e8f0e20d79b859b77ddb94b2a073c
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-05-04 14:11:31 -0700

    linked list kth'

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -99,7 +99,7 @@ Can properly return a collection of all the values that exist in the linked list
 
 
 
-    def kth_from_end(self, k=0):
+    def kth_from_end(self,value, k=0):
         '''
            kth from end
            argument: a number, k, as a parameter.
