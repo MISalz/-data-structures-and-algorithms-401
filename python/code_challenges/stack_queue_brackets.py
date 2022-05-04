612b62c33cc25bbf4ee4a2592afa1678f0cc838c
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-05-04 16:15:48 -0700

    update readme

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -3,11 +3,14 @@ from data_structures.stack import Stack
 
 
 def multi_bracket_validation(value):
-
+    '''
+    Write a function called validate brackets
+    Arguments: string
+    Return: boolean
+    representing whether or not the brackets in the string are balanced
+    '''
     stack = []
-
     for data in value:
-
         if data == '(' or data == '[' or data == '{':
             stack.append(data)
         else:
@@ -15,7 +18,6 @@ def multi_bracket_validation(value):
             if not stack:
                 return False
             else:
-
                 top = stack[-1]
                 if data == ')' and top == '(' or \
                         data == ']' and top == '[' or \
