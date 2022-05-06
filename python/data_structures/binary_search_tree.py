6796afbb46c0298c3cab4297f5f24ff44a22b08b
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-05-06 16:26:54 -0700

    trees

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -3,7 +3,18 @@ from data_structures.binary_tree import BinaryTree
 
 class BinarySearchTree(BinaryTree):
     """
-    Put docstring here
+    Create a Binary Search Tree class
+This class should be a sub-class (or your languages equivalent) of the Binary Tree Class, with the following additional methods:
+
+Add
+Arguments: value
+Return: nothing
+
+Adds a new node with that value in the correct location in the binary search tree.
+
+Contains
+Argument: value
+Returns: boolean indicating whether or not the value is in the tree at least once.
     """
 
     def __init__(self):
