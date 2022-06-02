0d2e340d12bd7aa56ad2a3eed161c6124ea36967
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-06-01 20:23:02 -0700

    fix

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,26 @@
 from data_structures.hashtable import Hashtable
 
 
-def left_join():
-    pass
+def left_join(table_a, table_b):
+    joined_table = []
+    # setting an empty list to append to
+
+    for key, value in table_a.items():
+        if table_b.get(key):
+            joined_table.append([key, value, table_b.get(key)])
+    # for the key and value in the first table cal the items
+    # append the kaye, value, and table_b's key to the joined_table
+    # continue until there is nothing left in either table
+    # if key exists get the key and the value and append it
+
+        else:
+            joined_table.append([key, value, 'NONE'])
+    # if else, append to the table and replace the key with None
+
+
+    print(joined_table)
+    return joined_table
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
--- python/code_challenges/insertion_sort.py
+++ python/code_challenges/insertion_sort.py
@@ -5,19 +5,19 @@ def insertion_sort(num_arr):
   for idx_current in range(len(num_arr)+1):
     # if idx in num_arr
     temp = num_arr[idx_current]
-    print(f"***temp's current index: {idx_current}\n***")
-    print(f"***temp's current array value: {temp}\n***")
+    # print(f"***temp's current index: {idx_current}\n***")
+    # print(f"***temp's current array value: {temp}\n***")
     # assign temp to idx
 
     next_idx = idx_current+1
-    print(f"Next_idx index: {next_idx}\n")
+    # print(f"Next_idx index: {next_idx}\n")
     #assign at next index after initial iteration
-    while next_idx > 0 and temp < num_arr[next_idx]:
+    while next_idx >= 0 and temp < num_arr[next_idx]:
         # print ("Next_idx index value: {next_idx}")
-        num_arr[next_idx+1] = num_arr[next_idx]
+        num_arr[next_idx - 1] = num_arr[next_idx]
         next_idx -= 1
         num_arr[next_idx+1] = temp
-        print(f">> temp's current value: {temp}\n ****")
+        # print(f">> temp's current value: {temp}\n ****")
 
   return num_arr
 
